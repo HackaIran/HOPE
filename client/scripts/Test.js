18ee624303ba4a9b80fc68da53e7adb7e758b4b5
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-07-19 01:59:27 +0430

    adviser's link has been fixed

--- client/scripts/Test.js
+++ client/scripts/Test.js
@@ -170,7 +170,7 @@ class Test {
                 fixIcon.setAttribute("target","_blank");
                 // fixIcon.classList.add('soon')
                 if(/license/i.test(tip.heading)){
-                    fixIcon.href = 'http://hackairan.com/hope/adviser';
+                    fixIcon.href = 'http://hope.hackairan.com/adviser';
                 }else{
                     fixIcon.style.display = 'none'
                 }
